d27f50f3e1d3aeee7d59579da3a383a19ab95cd3
Philipp A. Baer <philipp.baer@dfki.de>
2009-12-03 10:21:52 +0100

    Removed tabs

--- src/aastra.cs
+++ src/aastra.cs
@@ -38,18 +38,18 @@ using Castor.Dynamic;
 
 namespace Spica
 {
-	using ROS;
+    using ROS;
 
     public class Aastra
     {
-		protected const string GENERATOR_NAME = "aastra2";
-		protected const string GENERATOR_VERSION = "1.9.1";
-		protected static readonly string[] GENERATOR_COPYRIGHT = {
-			"2009 Spica Robotics Project (http://spica-robotics.net/)",
-			"2009 DFKI RIC Bremen (http://robotik.dfki-bremen.de/)",
-			"2005-2009 DAS-Lab, University of Kassel (http://das-lab.net/)"
-		};
-		protected const string GENERATOR_ID = GENERATOR_NAME + "/" + GENERATOR_VERSION;
+        protected const string GENERATOR_NAME = "aastra2";
+        protected const string GENERATOR_VERSION = "1.9.1";
+        protected static readonly string[] GENERATOR_COPYRIGHT = {
+            "2009 Spica Robotics Project (http://spica-robotics.net/)",
+            "2009 DFKI RIC Bremen (http://robotik.dfki-bremen.de/)",
+            "2005-2009 DAS-Lab, University of Kassel (http://das-lab.net/)"
+        };
+        protected const string GENERATOR_ID = GENERATOR_NAME + "/" + GENERATOR_VERSION;
 
         protected bool show_stats = false;
         protected string root_path = null;
@@ -60,18 +60,18 @@ namespace Spica
         protected List<string> template_targets = null;
         protected IDictionary<string, IList<TemplateInfo>> templates = null;
 
-		protected SystemConfig sc = new SystemConfig();
+        protected SystemConfig sc = new SystemConfig();
 
-		protected DateTime start;
-		protected DateTime startParse;
-		protected DateTime stopParse;
-		protected DateTime startTransform;
-		protected DateTime stopTransform;
-		protected DateTime startGenerate;
+        protected DateTime start;
+        protected DateTime startParse;
+        protected DateTime stopParse;
+        protected DateTime startTransform;
+        protected DateTime stopTransform;
+        protected DateTime startGenerate;
 
-		protected ArrayList locatedProperties = new ArrayList();
+        protected ArrayList locatedProperties = new ArrayList();
 
-		protected MsgSwitch trace = new MsgSwitch(MsgLevel.Info);
+        protected MsgSwitch trace = new MsgSwitch(MsgLevel.Info);
 
         protected Queue<ElementInfo> process_elements = null;
         protected struct ElementInfo
@@ -95,13 +95,13 @@ namespace Spica
             public TemplateInfo(string ext, TypeMap type_map, StringTemplateGroup stg)
             {
                 extension = ext;
-				map = type_map;
+                map = type_map;
                 group = stg;
             }
 
             public StringTemplate GetTemplate(Element element, string filename)
             {
-				SpicaML.Map = map;
+                SpicaML.Map = map;
 
                 StringTemplate template = (StringTemplate)group.GetInstanceOf("main");
                 template.PassThroughAttributes = true;
@@ -127,74 +127,74 @@ namespace Spica
             }
 
             public string extension;
-			public TypeMap map;
+            public TypeMap map;
             public StringTemplateGroup group;
         }
 
-		protected void PrintVersionInformation()
-		{
-			Console.WriteLine("Spica Robotics {0} {1}", GENERATOR_NAME, GENERATOR_VERSION);
-			foreach (string copy in GENERATOR_COPYRIGHT)
-			{
-				Console.WriteLine("* {0}", copy);
-			}
-			Debug.WriteLine("Root directory:     {0}", this.root_path);
-			Debug.WriteLine("Template directory: {0}", this.template_path);
-			if (this.output_path != null)
-			{
-				Debug.WriteLine("Output directory:   {0}", this.output_path);
-			}
-		}
-
-		public Aastra(string[] args)
+        protected void PrintVersionInformation()
         {
-			this.start = DateTime.UtcNow;
+            Console.WriteLine("Spica Robotics {0} {1}", GENERATOR_NAME, GENERATOR_VERSION);
+            foreach (string copy in GENERATOR_COPYRIGHT)
+            {
+                Console.WriteLine("* {0}", copy);
+            }
+            Debug.WriteLine("Root directory:     {0}", this.root_path);
+            Debug.WriteLine("Template directory: {0}", this.template_path);
+            if (this.output_path != null)
+            {
+                Debug.WriteLine("Output directory:   {0}", this.output_path);
+            }
+        }
+
+        public Aastra(string[] args)
+        {
+            this.start = DateTime.UtcNow;
 
             DeterminePaths();
             string target_string = DetermineTargets();
 
-			Arguments a = new Arguments(1, -1);
-			a.SetOption("generate!", "Generate code for the given platforms (" + target_string + ")");
+            Arguments a = new Arguments(1, -1);
+            a.SetOption("generate!", "Generate code for the given platforms (" + target_string + ")");
             a.SetOption("?output:autogen", "Output directory for generated code, for each target a separate subdirectory is created");
-			a.SetOption("?templates", "Directory to search for templates (" + this.template_path + ")");
-			a.SetOption("?show-ast", "Show AST after parsing the spec");
+            a.SetOption("?templates", "Directory to search for templates (" + this.template_path + ")");
+            a.SetOption("?show-ast", "Show AST after parsing the spec");
             a.SetOption("?show-structs", "Show extracted structures");
             a.SetOption("?show-enums", "Show extracted structures");
             a.SetOption("?show-modules", "Show extracted structures");
             a.SetOption("?show-stats", "Show statistics");
-			a.SetOption("?ros-messages", "Process messages defined in ROS");
-			a.SetOption("?ros-package-path!", "Path to ROS packages (will be searched recursively)");
+            a.SetOption("?ros-messages", "Process messages defined in ROS");
+            a.SetOption("?ros-package-path!", "Path to ROS packages (will be searched recursively)");
             a.SetOption("?flat", "Do not store generated code in type-secific subdirectories (enum, struct, module)");
             a.SetOption("?verbose", "Be verbose, output everything");
             a.SetOption("?quiet", "Suppress all output");
 
-			// Consume the arguments
-			try {
-				a.Consume(args);
-			} catch (Exception e) {
-				PrintVersionInformation();
-				Console.Error.Write(a.ToString());
-				Debug.WriteLine(e.Message);
-				Debug.WriteLine("Add one or more specification files");
-				Environment.Exit(1);
-			}
+            // Consume the arguments
+            try {
+                a.Consume(args);
+            } catch (Exception e) {
+                PrintVersionInformation();
+                Console.Error.Write(a.ToString());
+                Debug.WriteLine(e.Message);
+                Debug.WriteLine("Add one or more specification files");
+                Environment.Exit(1);
+            }
 
-			// Check the parameter count
-			if (a.GetParameters().Count < 1) {
-				PrintVersionInformation();
-				Console.Error.Write(a.ToString());
-				Debug.WriteLine("No input file given!");
-				Debug.WriteLine("And one or more specification files");
-				Environment.Exit(1);
-			}
+            // Check the parameter count
+            if (a.GetParameters().Count < 1) {
+                PrintVersionInformation();
+                Console.Error.Write(a.ToString());
+                Debug.WriteLine("No input file given!");
+                Debug.WriteLine("And one or more specification files");
+                Environment.Exit(1);
+            }
 
-			bool verbose = a.OptionIsSet("verbose");
-			bool quiet = a.OptionIsSet("quiet");
+            bool verbose = a.OptionIsSet("verbose");
+            bool quiet = a.OptionIsSet("quiet");
             bool show_enums = a.OptionIsSet("show-enums");
             bool show_structs = a.OptionIsSet("show-structs");
             bool show_modules = a.OptionIsSet("show-modules");
 
-			this.show_stats = a.OptionIsSet("show-stats");
+            this.show_stats = a.OptionIsSet("show-stats");
             this.output_path = Path.GetFullPath(a.GetOptionValues("output")[0]);
             this.output_flat = a.OptionIsSet("flat");
 
@@ -215,31 +215,31 @@ namespace Spica
 
             this.trace.Level = (quiet ? MsgLevel.Off : (verbose ? MsgLevel.Verbose : MsgLevel.Info));
 
-			if (!quiet)
+            if (!quiet)
             {
-				PrintVersionInformation();
-			}
+                PrintVersionInformation();
+            }
 
             DateTime overall_start = DateTime.UtcNow;
 
-			if (a.OptionIsSet("ros-messages"))
-			{
-				ROS.ROS ros = new ROS.ROS();
-
-				if (a.OptionIsSet("ros-package-path"))
-				{
-					ros.AddPackagePath(Path.GetFullPath(a.GetOptionValues("ros-package-path")[0]));
-				}
-
-				ros.Scan();
-			}
-
-			try
+            if (a.OptionIsSet("ros-messages"))
             {
-				// Generate the parse tree
-				IList<SpicaML> specs = Load(a.GetParameters().ToArray());
+                ROS.ROS ros = new ROS.ROS();
 
-				if (a.OptionIsSet("show-ast"))
+                if (a.OptionIsSet("ros-package-path"))
+                {
+                    ros.AddPackagePath(Path.GetFullPath(a.GetOptionValues("ros-package-path")[0]));
+                }
+
+                ros.Scan();
+            }
+
+            try
+            {
+                // Generate the parse tree
+                IList<SpicaML> specs = Load(a.GetParameters().ToArray());
+
+                if (a.OptionIsSet("show-ast"))
                 {
                     foreach (SpicaML sml in specs)
                     {
@@ -247,9 +247,9 @@ namespace Spica
                         Console.WriteLine(sml.AST.ToString());
                         Console.WriteLine();
                     }
-				}
+                }
 
-				if (show_structs || show_enums || show_modules)
+                if (show_structs || show_enums || show_modules)
                 {
                     foreach (SpicaML sml in specs)
                     {
@@ -288,11 +288,11 @@ namespace Spica
                 // Generate the code
                 Generate(targets, specs);
 
-			}
+            }
             catch (Exception e)
             {
-        		DisplayException(e);
-			}
+                DisplayException(e);
+            }
 
             DateTime overall_end = DateTime.UtcNow;
 
@@ -300,35 +300,35 @@ namespace Spica
             {
                 Trace.WriteLine("Overall runtime {0} ms", (overall_end - overall_start).Ticks / 10000);
             }
-		}
+        }
 
-		protected uint ComputeHash(byte[] data)
+        protected uint ComputeHash(byte[] data)
         {
-			const uint p = 16777619;
-			uint hash = 2166136261;
+            const uint p = 16777619;
+            uint hash = 2166136261;
 
-			foreach (byte b in data)
+            foreach (byte b in data)
             {
-				hash = (hash ^ b) * p;
-			}
+                hash = (hash ^ b) * p;
+            }
 
-			hash += hash << 13;
-			hash ^= hash >> 7;
-			hash += hash << 3;
-			hash ^= hash >> 17;
-			hash += hash << 5;
+            hash += hash << 13;
+            hash ^= hash >> 7;
+            hash += hash << 3;
+            hash ^= hash >> 17;
+            hash += hash << 5;
 
-			return hash;
-		}
+            return hash;
+        }
 
-		/**
-		 * Loads the given SpicaML specifications
-		 * @param files The files that should be loaded
-		 * @return A list of SpicaML instances
-		 */
-		protected IList<SpicaML> Load(IList<string> files)
+        /**
+         * Loads the given SpicaML specifications
+         * @param files The files that should be loaded
+         * @return A list of SpicaML instances
+         */
+        protected IList<SpicaML> Load(IList<string> files)
         {
-			IList<SpicaML> result = new List<SpicaML>();
+            IList<SpicaML> result = new List<SpicaML>();
 
             foreach (string file in files)
             {
@@ -348,8 +348,8 @@ namespace Spica
                 result.Add(parser);
             }
 
-			return result;
-		}
+            return result;
+        }
 
         protected void DeterminePaths()
         {
@@ -409,8 +409,8 @@ namespace Spica
             {
                 string path = Path.Combine(this.template_path, t);
 
-				// Load type mapping
-				TypeMap type_map = new TypeMap(path);
+                // Load type mapping
+                TypeMap type_map = new TypeMap(path);
 
                 // Get available templates and extensions
                 IList<string> files = Directory.GetFiles(path, "*.stg");
@@ -427,7 +427,7 @@ namespace Spica
                     string filename = Path.GetFileNameWithoutExtension(file);
                     int dot = filename.LastIndexOf('.');
 
-					Console.WriteLine("---> " + filename);
+                    Console.WriteLine("---> " + filename);
 
                     string name = filename.Substring(0, dot);
                     string ext = filename.Substring(dot + 1, filename.Length - (dot + 1));
@@ -513,45 +513,45 @@ namespace Spica
 
             DateTime generate_start = DateTime.UtcNow;
 
-			IList<Thread> thread_pool = new List<Thread>();
-			for (int i = 0; i < System.Environment.ProcessorCount + 1; i++)
+            IList<Thread> thread_pool = new List<Thread>();
+            for (int i = 0; i < System.Environment.ProcessorCount + 1; i++)
             {
-				thread_pool.Add(new Thread(ProcessElements));
-			}
+                thread_pool.Add(new Thread(ProcessElements));
+            }
 
-			Trace.WriteLineIf(this.trace.Verbose, "Created {0} worker threads for {1} jobs", thread_pool.Count, count);
+            Trace.WriteLineIf(this.trace.Verbose, "Created {0} worker threads for {1} jobs", thread_pool.Count, count);
 
             // Start all threads
-			foreach (Thread thread in thread_pool)
+            foreach (Thread thread in thread_pool)
             {
-				thread.Start();
-			}
+                thread.Start();
+            }
 
             // Wait for threads to be finished
-			foreach (Thread thread in thread_pool)
+            foreach (Thread thread in thread_pool)
             {
-				thread.Join();
-			}
+                thread.Join();
+            }
 
             DateTime generate_end = DateTime.UtcNow;
 
             Trace.WriteLine("Generated {0} files in {1} ms", count, (generate_end - generate_start).Ticks / 10000);
-		}
+        }
 
-		protected void ProcessElements()
+        protected void ProcessElements()
         {
-			ElementInfo ei;
+            ElementInfo ei;
 
-			for (;;)
+            for (;;)
             {
-				lock (this.process_elements)
+                lock (this.process_elements)
                 {
-					if (this.process_elements.Count == 0)
+                    if (this.process_elements.Count == 0)
                     {
                         return;
                     }
 
-					ei = this.process_elements.Dequeue();
+                    ei = this.process_elements.Dequeue();
 
                     string output = Path.Combine(this.output_path, ei.target);
 
@@ -602,7 +602,7 @@ namespace Spica
                         fs.Write(data, 0, data.Length);
                         fs.Close();
                     }
-				}
+                }
             }
         }
     
@@ -617,9 +617,9 @@ namespace Spica
             }
         }
 
-		public static void Main(string[] args)
+        public static void Main(string[] args)
         {
-			new Aastra(args);
-		}
-	}
+            new Aastra(args);
+        }
+    }
 }
